44fbf91506834432b9de34fbdfd4ae9bb8a31fb9
DylanMrn <dy.marion92@gmail.com>
2021-04-04 22:37:09 +0200

    [Dylan] maj 04/04

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
-{ useNewUrlParser: true,
-useUnifiedTopology: true })
-.then(() => console.log('Connexion à MongoDB réussie !'))
-.catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 const app = express();
 
